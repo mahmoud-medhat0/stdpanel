e6d2d2402a2475178a303b393b5740e2fc786876
Mahmoud Medhat <hhetlar120@gmail.com>
2022-10-16 16:11:52 +0200

    disable login/reg

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,12 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Auth::routes([
+    'register' => false, // Registration Routes...
+    'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
+  ]);
+  
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
