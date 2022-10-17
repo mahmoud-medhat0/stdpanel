807f786dd39f61dab5d469cd2253f53fb8f9c2bc
Mahmoud Medhat <hhetlar120@gmail.com>
2022-10-17 15:25:18 +0200

    12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -115,9 +115,47 @@
 
 </html>
 @endguest
-@extends('layouts.parent2')
+<head>
 
-@section('content')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <link rel="icon" href="{{ asset('img/mini_logo.png')}}')}}" type="image/png">
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap1.min.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/themefy_icon/themify-icons.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/niceselect/css/nice-select.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/owl_carousel/css/owl.carousel.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/gijgo/gijgo.min.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/font_awesome/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('vendors/tagsinput/tagsinput.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/datepicker/date-picker.css')}}">
+    <link rel="stylesheet" href="{{ asset('vendors/vectormap-home/vectormap-2.0.2.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/scroll/scrollable.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/datatable/css/jquery.dataTables.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('vendors/datatable/css/responsive.dataTables.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('vendors/datatable/css/buttons.dataTables.min.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/text_editor/summernote-bs4.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/morris/morris.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('vendors/material_icon/material-icons.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('css/metisMenu.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('css/style1.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/colors/default.css')}}" id="colorSkinCSS">
+</head>
+
 <center>
     <div class="col-lg-6">
         <div class="create_report_btn mt_30">
@@ -140,4 +178,3 @@
         </div>
     </div>
 </center>
-@endsection
